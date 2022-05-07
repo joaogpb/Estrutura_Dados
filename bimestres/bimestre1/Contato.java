4bbf1fde08cadbda4a40a4a9437a2b928cd97b46
joaogpb <81706330+joaogpb@users.noreply.github.com>
2022-05-07 18:19:54 -0300

    Update Contato.java

--- bimestres/bimestre1/Contato.java
+++ bimestres/bimestre1/Contato.java
@@ -1,98 +1,66 @@
-import java.util.ArrayList;
+
+import java.util.List;
 import java.util.Scanner;
-//import java.util.Iterator;
-import java.util.Collections;
+import java.util.jar.Attributes.Name;
 
-public class App {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+public class Contato extends Exception {
+    private int id;
 
-        ArrayList<Contato> lista = new ArrayList<>();
+    private String nome;
 
-        boolean maisJogadores = false; 
+    private String telefone;
 
-        while(true) {
-            System.out.println("Menu de opções");
-            System.out.println("1 - Incluir contato");
-            System.out.println("2 - Alterar contato");
-            System.out.println("3 - Desfazer última alteração do contato");
-            System.out.println("4 - Excluir contato");
-            System.out.println("5 - Listar contatos");
-            System.out.println("6 - Sair");
-
-            int opcao = scan.nextInt();
-
-            switch (opcao) {
-                case 1:
-                    System.out.println("Informe o nome do Contato: ");
-                    String nome = scan.next();
-
-                    System.out.println("Informe o  Email: ");
-                    String email = scan.next();
+    private String email;
         
-                    System.out.println("Informe o  telefone: ");
-                    int telefone = scan.nextInt();
+    private static int CONTADOR = 0;
+
+    public static PilhaEstatica historico = new PilhaEstatica(10);
+
+
+    public Contato(String nome , String telefone, String email) {
+        this.setNome(nome);
+        this.setTelefone(telefone);
+        this.setEmail(email);
         
-                    Contato contato = new Contato(nome,email,telefone);
-                    lista.add(contato);
-                    break;
-                case 2:
+        Contato.CONTADOR++;
+        this.setId(Contato.CONTADOR);
+    }
 
-                    break;
-                
-                case 3:
+    public void Global(){
 
-                    break;
-                case 4:
-
-                    break;
-                case 5:
-                    for (Contato c : lista) {
-                        System.out.println("Id: " + c.getId());
-                        System.out.println("Nome: " + c.getNome());
-                        System.out.println("Email: " + c.getEmail());
-                        System.out.println("telefone: " + c.getTelefone());
-                    }
-                    break;    
-                case 6:
-                    System.exit(0);
-                    break;
-            
-                default:
-                    System.out.println("Opção inválida");
-                    break;
-            }
-        }
-        
-/*         do {
-            System.out.println("Informe o nome do Contato: ");
-            String nome = scan.next();
-
-            System.out.println("Informe o  Email: ");
-            String email = scan.next();
-
-            System.out.println("Informe o  telefone: ");
-            int telefone = scan.nextInt();
-
-            System.out.println("Informe o Id da pessoa: ");
-            int id = scan.nextInt();
-            
-            
+    }
 
 
-            
-        } while (maisJogadores);
+    public int getId() {
+        return id;
+    }
 
-        scan.close();
+    public void setId(int id) {
+        this.id = id;
+    }
 
-        //Ordenar o ranking
-        Collections.sort(lista);
+    public String getEmail() {
+        return email;
+    }
 
-        System.out.println("----------- lista -----------");
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
+    public String getTelefone() {
+        return telefone;
+    }
 
-        for (Contato game: lista) {
-             System.out.println(game);
-        } */
-    }    
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
 }
