45ebc3b6242f0a9fedd1eab698c6221c760f9867
joaogpb <81706330+joaogpb@users.noreply.github.com>
2022-05-07 18:19:28 -0300

    Update App.java

--- bimestres/bimestre1/App.java
+++ bimestres/bimestre1/App.java
@@ -1,22 +1,23 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-//import java.util.Iterator;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Collections;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws PilhaVaziaException, PilhaCheiaException, Contato{
         Scanner scan = new Scanner(System.in);
 
         ArrayList<Contato> lista = new ArrayList<>();
 
-        boolean maisJogadores = false; 
+        boolean maisContatos = false; 
 
         while(true) {
             System.out.println("Menu de opções");
             System.out.println("1 - Incluir contato");
             System.out.println("2 - Alterar contato");
             System.out.println("3 - Desfazer última alteração do contato");
-            System.out.println("4 - Excluir contato");
+            System.out.println("4 - Excluir todos os contatos");
             System.out.println("5 - Listar contatos");
             System.out.println("6 - Sair");
 
@@ -31,68 +32,39 @@ public class App {
                     String email = scan.next();
         
                     System.out.println("Informe o  telefone: ");
-                    int telefone = scan.nextInt();
+                    String telefone = scan.next();
         
                     Contato contato = new Contato(nome,email,telefone);
                     lista.add(contato);
+                    
                     break;
                 case 2:
-
+                    
                     break;
-                
                 case 3:
-
+                    
                     break;
                 case 4:
-
+                    for(int i = 0; i <= 11; i++){
+                        Contato.historico.pop();
+                    }
+                    System.out.println(Contato.historico.pop());
                     break;
                 case 5:
                     for (Contato c : lista) {
                         System.out.println("Id: " + c.getId());
                         System.out.println("Nome: " + c.getNome());
                         System.out.println("Email: " + c.getEmail());
-                        System.out.println("Email: " + c.getTelefone());
+                        System.out.println("telefone: " + c.getTelefone());
                     }
                     break;    
                 case 6:
                     System.exit(0);
-                    break;
-            
+                    break;          
                 default:
                     System.out.println("Opção inválida");
                     break;
             }
         }
-        
-/*         do {
-            System.out.println("Informe o nome do Contato: ");
-            String nome = scan.next();
-
-            System.out.println("Informe o  Email: ");
-            String email = scan.next();
-
-            System.out.println("Informe o  telefone: ");
-            int telefone = scan.nextInt();
-
-            System.out.println("Informe o Id da pessoa: ");
-            int id = scan.nextInt();
-            
-            
-
-
-            
-        } while (maisJogadores);
-
-        scan.close();
-
-        //Ordenar o ranking
-        Collections.sort(lista);
-
-        System.out.println("----------- lista -----------");
-
-
-        for (Contato game: lista) {
-             System.out.println(game);
-        } */
-    }    
+    }
 }
