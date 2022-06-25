efd839369718854c0c467d9aca94458f1445e387
joaogpb <81706330+joaogpb@users.noreply.github.com>
2022-06-24 21:58:49 -0300

    Update Prova.java

--- Provas/2b/Prova.java
+++ Provas/2b/Prova.java
@@ -1,1 +1,22 @@
-11
+package prova1;
+
+
+import java.util.List;
+
+public class Leitor {
+    public Prova contador;
+    private Object vetor;
+
+    public void push(){
+        vetor = contador;
+
+
+        while(contador != 0){
+            vetor += contador;
+        }
+    };
+
+    public static void main(String[] args) {
+        
+    }
+};
